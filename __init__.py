719cf9d9f9b2929861ce2922fb4d774702cce4b6
kareemMohamed <k.mohamed@theelephant.tech>
2018-09-15 11:17:38 +0200

    change host

--- __init__.py
+++ __init__.py
@@ -313,5 +313,5 @@ def showMovieJSON(id):
 
 if __name__ == '__main__':
     app.secret_key = 'Item55626Catalog'
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    # app.debug = True
+    app.run()
